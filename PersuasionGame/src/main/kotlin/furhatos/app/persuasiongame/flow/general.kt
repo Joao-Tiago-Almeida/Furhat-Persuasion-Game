c0dc42406bd16f8cd2fa579799e91fe3db9b8799
Philipp <philipp.mondorf@googlemail.com>
2022-01-11 10:57:04 +0100

    added buttons

--- PersuasionGame/src/main/kotlin/furhatos/app/persuasiongame/flow/general.kt
+++ PersuasionGame/src/main/kotlin/furhatos/app/persuasiongame/flow/general.kt
@@ -63,4 +63,24 @@ val Interaction: State = state {
         goto(GameOver)
     }
 
+    onButton("Yes") {
+        random(
+            { furhat.ask("Yes!") },
+            { furhat.ask("Yes, ${users.current.name}.") }
+        )
+    }
+
+    onButton("No") {
+        random(
+            { furhat.ask("No!") },
+            { furhat.ask("No, ${users.current.name}.") }
+        )
+    }
+
+    onButton("Say Question Number") {
+        random(
+            { furhat.ask("Sorry, please tell me the question's number when asking for help.") },
+            { furhat.ask("Sorry, ${users.current.name}. Could you tell me the question number first?") }
+        )
+    }
 }
