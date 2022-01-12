605627dd2d3e0f71545a10096419e484791be7e6
Joao-Tiago-Almeida <joao.marques.almeida@tecnico.ulisboa.pt>
2022-01-12 20:57:23 +0100

    graphics done

--- PersuasionGame/results/process_results.py
+++ PersuasionGame/results/process_results.py
@@ -8,7 +8,7 @@ import matplotlib.patches as mpatches
 def get_data():
 
     results = {}
-    folder = "PersuasionGame/results/"
+    folder = "PersuasionGame/results/outputs/"
 
     for file in os.listdir(folder):
 
@@ -21,6 +21,7 @@ def get_data():
         for s in range(len(info)):
             fields = info[s].replace("\n","").split(": ")
 
+            if fields[0] == '' or fields[0] == ' ': continue
             if fields[1] == "null": break
 
             try:
@@ -35,7 +36,7 @@ def get_data():
 
     return results
 
-def plot_unhetical_decisions_per_mode(df):
+def plot_unhetical_decisions_per_mode(df, title:str):
 
     df2 = pd.DataFrame(dtype=int64)
     
@@ -51,8 +52,9 @@ def plot_unhetical_decisions_per_mode(df):
     df2 = df2.reindex(sorted(df2.columns, reverse=True), axis=1)
 
     fig = plt.figure()
+    fig.suptitle("Relation Between Unethical Decisions taken per Robot Mode")
     boxplot = df2.boxplot(grid=True, rot=0)
-    boxplot.set_title("Relation Between Unethical Decisions taken per Robot Mode")
+    boxplot.set_title(title)
     boxplot.set_ylabel("Number of Unethical Decisions")
 
     leg = []
@@ -64,29 +66,29 @@ def plot_unhetical_decisions_per_mode(df):
     boxplot.legend(handles=leg, loc=2)
     boxplot.get_legend().set_title("Average Number of\nUnethical Desicions")
 
-    fig.savefig('PersuasionGame/results/unethical_mode.png')
+    fig.savefig(f'PersuasionGame/results/unethical_mode_[{title.replace(" ","_").lower()}].png')
 
     breakpoint
 
 def plot_mode_distribution(df):
 
-    x_label = list(df['Robot mode'].unique())
-    y_label = list(df['Robot mode'].value_counts())
+    index = sorted(df['Robot mode'].unique(), reverse=False)
+    remote = list(df[df['Conditions']=="Virtual"]['Robot mode'].value_counts().sort_index(ascending=False))
+    in_person = list(df[df['Conditions']=="In Person"]['Robot mode'].value_counts().sort_index(ascending=False))
 
     df2 = pd.DataFrame(
-        {'Robot mode': x_label, 'Occurances': y_label}
+        {'Remote': remote, "In Person": in_person},
+        index = index
     )
-    df2.sort_values(by='Robot mode', inplace=True, ascending=False)
 
-    bar = df2.plot.bar(
-        x='Robot mode', 
-        y='Occurances', 
+    df2.plot.bar(
         rot=0, 
         xlabel="", 
         ylabel="Occurances", 
-        legend=False, 
-        title="Number of experiments performed with each Robot mode"
-        ).get_figure().savefig('PersuasionGame/results/occurrences_mode.png')
+        legend=True, 
+        stacked=True,
+        title="Number of experiments performed with each Robot mode" ,
+        ).get_figure().savefig(f'PersuasionGame/results/occurrences_mode.png')
 
     breakpoint
 
@@ -97,8 +99,11 @@ if __name__ == "__main__":
 
     df = pd.DataFrame.from_dict(data)
 
-    plot_unhetical_decisions_per_mode(df)
+    plot_unhetical_decisions_per_mode(df[df["Conditions"]=="In Person"], "(In Person Experiments)")
 
+    plot_unhetical_decisions_per_mode(df[df["Conditions"]=="Virtual"], "(Remote Experiments)")
+
+    plot_unhetical_decisions_per_mode(df, "(All Experiments)")
     plot_mode_distribution(df)
 
     print(f'Number of experiments performed: {len(df)}.')
